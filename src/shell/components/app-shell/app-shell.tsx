1c6e92538ae674aaa27c938a8c25c8dac36f7240
Cristian Levrero <cristian.levrero@dialpad.com>
2023-03-07 11:19:21 -0300

    first commit

---
+++ src/shell/components/app-shell/app-shell.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function AppShell() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+        hola guacho
+    </div>
+  )
+}
+
+export default AppShell
